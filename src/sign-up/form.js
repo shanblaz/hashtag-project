553f44de7bcc1bd96408becb8c0106e77da91c88
Shan <m.shan@cankado.com>
2021-01-04 19:43:52 +0530

    fixed

---
+++ src/sign-up/form.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+
+export default class Form extends Component {
+    render() {
+        return (
+            <div>
+                <form>
+                    <div class="form-row">
+                        <div class="col form-group">
+                            <input type="text" class="form-control" placeholder="Name"/>
+                        </div>
+                        <div class="col form-group">
+                            <input type="text" class="form-control" placeholder="User Name"/>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <input type="email" class="form-control" placeholder="Email"/>
+                    </div>
+                    <div class="form-group">
+                        <input type="date" class="form-control" placeholder="Date of birth"/>
+                    </div> 
+                    <div class="form-group">
+                        <input type="number" class="form-control"placeholder="Phone"/>
+                    </div>  
+                    <div class="form-group">
+                        <input type="password" class="form-control" placeholder="Password"/>
+                    </div>  
+                    <div className="form-check mb-3 text-left">
+                        <input className="form-check-input" type="checkbox"/>
+                        <label class="form-check-label">Accept Terms of Service, Privacy Policy</label>
+                    </div>                                      
+                        <div class="form-group">
+                            <button style={{borderRadius: "20px", backgroundColor: "#e04a63"}} type="submit" class="btn btn-primary btn-block"> SIGN UP  </button>
+                        </div>   
+                        <small class="text-muted">Already a member?</small><span> Sign In</span>                                          
+                </form>
+            </div>
+        )
+    }
+}
---
+++ src/sign-up/index.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import './signup.css'
+import Form from './form'
+export default class SignUp extends Component {
+  render() {
+    return (
+    <div className="form_part container-fluid h-100">
+        <div className="row h-100">
+            <div className="col-md-5 d-md-flex d-none p-0">
+                <div style={{overflow: 'hidden'}}>
+                    <img style={{minWidth: '100%', minHeight: '100%', transform: 'translateX(-20%)'}} src="/images/pic.jpeg"/>
+                </div>
+            </div>
+            <div className="col-12 col-md-7 d-flex flex-column h-100 p-0">
+                <h3 className="my-3 text-right heading px-2">WATCHNEXT</h3>
+                <div style={{width: "50%"}}className="align-self-center form_contents p-4">
+                    <div className="text-center">
+                    {/* <div className="row justify-content-center "> </div> */}
+                        <h3 className="msg-info mb-4">Sign Up</h3>
+                        <Form/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    )
+  }
+}
